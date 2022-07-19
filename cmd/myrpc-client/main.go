ff9b117752f5c47f3b58d8e629f63df0c3ba2fd2
Mario Ponce <433814+mponce@users.noreply.github.com>
2022-07-18 22:02:54 -0700

    implementing rpc client

---
+++ cmd/myrpc-client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	pb "github.com/mponce/myproto/go-proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+
+	conn, err := grpc.Dial(":9090", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewPingClient(conn)
+ 
+	response, err := c.SayHello(context.Background(), &pb.PingMessage{Greeting: "foo"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Greeting)
+}
+
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/mponce/myrpc-client
+
+go 1.18
+
+require (
+	github.com/mponce/myproto v0.0.0-20220718154355-270b78275f18
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129
+	google.golang.org/grpc v1.48.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220718134204-073382fd740c // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
